c01e5df4339d5fb7304a9ae5ca3b9ae99bf07128
victor <victor.miguel@devio.com.br>
2022-07-10 20:33:04 -0300

    deploy fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import Image from 'Next/image'
-import logo from '../../public/logo.png'
+import React from "react";
+import Image from "next/image";
+import logo from "../../public/logo.png";
 
 export default function Header() {
-  return (
-    <div className='w-screen h-12 line-center bg-rose-300 fixed z-50'>
-      <Image src={logo} width={75} height={30} />
-    </div>
-  )
+    return (
+        <div className="w-screen h-12 line-center bg-rose-300 fixed z-50">
+            <Image src={logo} width={75} height={30} />
+        </div>
+    );
 }
